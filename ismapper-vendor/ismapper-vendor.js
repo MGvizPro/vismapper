89f6aa1b4fef6ba159f9c8505d68ecad05be7e99
jmjuanes <josemijuanes@gmail.com>
2015-12-29 17:39:50 +0100

    Finish vendor script

--- ismapper-vendor/ismapper-vendor.js
+++ ismapper-vendor/ismapper-vendor.js
@@ -1,2 +1,14 @@
 //Import dependencies
 var stattic = require('stattic');
+
+//Import config
+var Config = require('../ismapper-config.json');
+
+//Set the folder with the static files
+stattic.set('static', './public');
+
+//Set the port
+stattic.set('port', Config.vendor.port);
+
+//Run the server
+stattic.run();
