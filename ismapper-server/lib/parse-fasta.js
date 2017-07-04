8eb69dda3c0d967d158b1b74cd94583b6de20579
jmjuanes <josemijuanes@gmail.com>
2017-07-04 16:19:56 +0200

    ismapper-server/lib/parse-fasta.js: renamed fasta parser. Big rewrite

---
+++ ismapper-server/lib/parse-fasta.js
@@ -0,0 +1,38 @@
+//Import dependencies
+var fs = require('fs');
+var fasta_tools = require('fasta-tools');
+
+//Parse a fasta/fastq file
+module.exports = function(input_fasta, output_fasta, cb)
+{
+	//Read the fasta or fastq file
+  fasta_tools.read(input_fasta,  function(error, obj)
+  {
+    //Check the error
+    if(error){ return cb(error, null); }
+
+    //Check the file format
+    if(obj.format === 'fasta')
+    {
+      //Convert to fastq
+      obj = fasta_tools.toFastQ(obj);
+    }
+
+    //Save the number of reads
+    var num_reads = obj.reads.length;
+
+    //Save the fastq file
+    return fasta_tools.write(output_fasta, obj, function(error)
+    {
+      //Check the error
+      if(error){ return cb(error, 0); }
+
+      //Delete the original file
+      return fs.unlink(input_fasta, function(error)
+      {
+        //Do the callback
+        return cb(error, num_reads);
+      });
+    });
+  });
+};
