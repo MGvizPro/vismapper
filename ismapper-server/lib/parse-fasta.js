cec27d0f1fb8c7256f105c43bd726e1f3c42614f
jmjuanes <josemijuanes@gmail.com>
2017-07-05 11:16:06 +0200

    ismapper-server/lib/parse-fasta.js: added default fastq quality

--- ismapper-server/lib/parse-fasta.js
+++ ismapper-server/lib/parse-fasta.js
@@ -5,25 +5,37 @@ var fasta_tools = require('fasta-tools');
 //Parse a fasta/fastq file
 module.exports = function(input_fasta, output_fasta, cb)
 {
+  //Display in console
+  console.log('Reading FASTA file ' + input_fasta);
+
 	//Read the fasta or fastq file
   fasta_tools.read(input_fasta,  function(error, obj)
   {
     //Check the error
     if(error){ return cb(error, null); }
 
+    //Display in console
+    console.log('FASTA file processed. Detected ' + obj.reads.length);
+
     //Check the file format
     if(obj.format === 'fasta')
     {
       //Convert to fastq
-      obj = fasta_tools.toFastQ(obj);
+      obj = fasta_tools.toFastQ(obj, 20);
     }
 
     //Save the number of reads
     var num_reads = obj.reads.length;
 
+    //Display in console
+    console.log('Saving FASTQ file as ' + output_fasta);
+
     //Save the fastq file
     return fasta_tools.write(output_fasta, obj, function(error)
     {
+      //Display in console
+      console.log('FASTQ file saved');
+      
       //Check the error
       if(error){ return cb(error, 0); }
 
